f8100a1e4ed2cad9c405c385ec44277a1e56a405
socratest3 <socratest3@nycstudents.net>
2025-01-08 12:14:18 -0500

    Update Sign.java

--- Sign.java
+++ Sign.java
@@ -1,1 +1,30 @@
-
+public class Sign
+{
+    private String message;
+    private int width;
+    Sign(String m, int w)
+    {
+        message = m;
+        width = w;
+    } 
+    public int numberOfLines()
+    {
+        int lines = message.length()/width;
+        if(message.length() % width == 0)
+        {return lines;}
+        else{return lines+1;}
+    }
+    public String getLines()
+    {
+        String s = "";
+        int i = 0;
+        if(message.length()==0)return null;
+        while (i<message.length())
+        {
+            s += message.substring(i,i+1);
+            i++;
+            if(i % width == 0 && i != message.length()) s += ";";
+        }
+        return s;
+    }
+}
